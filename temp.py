f3bda25222eff7f2bf6ff3bc71f6ceeabafc94c1
GitHubber2001 <kevin-kuipers@home.nl>
2026-02-19 12:43:48 +0100

    rng

--- temp.py
+++ temp.py
@@ -6,12 +6,17 @@ import numpy as np
 import pandas as pd
 from sklearn import model_selection
 
+RANDOM_SEED = 42
+np.random.seed(RANDOM_SEED)
 
 test_df = pd.read_json("data/test.jsonl", lines=True)
 train_df = pd.read_json("data/train.jsonl", lines=True)
 
 print(train_df)
-train_df, validation_df = model_selection.train_test_split(train_df, test_size=0.1, train_size=0.9)
+train_df, validation_df = model_selection.train_test_split(
+    train_df, random_state=RANDOM_SEED, test_size=0.1, train_size=0.9
+)
+
 
 def normalize_text(text: str) -> str:
     """Return the normalized version of a text"""
