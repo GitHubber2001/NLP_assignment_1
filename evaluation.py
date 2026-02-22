cda2f6fa01debd5071e4638429f97c7013808352
GitHubber2001 <kevin-kuipers@home.nl>
2026-02-22 21:34:19 +0100

    implemented evaluation and error analysis

---
+++ evaluation.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+from sklearn.metrics import (
+    ConfusionMatrixDisplay,
+    accuracy_score,
+    classification_report,
+    confusion_matrix,
+)
+
+
+def show_key_metrics(test_y, test_prediction, model_name: str) -> None:
+    target_names = ["label", "title", "description"]
+    labels = [0, 1, 2, 3]
+
+    report = classification_report(
+        test_y, test_prediction, labels=labels, target_names=target_names
+    )
+
+    cm = confusion_matrix(test_y, test_prediction)
+    confusion_matrix_display_regression = ConfusionMatrixDisplay(
+        confusion_matrix=cm, display_labels=labels
+    )
+
+    accuracy_classification_score = accuracy_score(
+        test_y, test_prediction, normalize=True
+    )
+
+    print(f"{model_name}:\n {report}")
+
+    print(
+        f"{model_name}: Accuracy classificaion score: {accuracy_classification_score}"
+    )
+
+    plt.title(f"Confusion Matrix | {model_name} with TF-IDF")
+    plt.show()
--- main.py
+++ main.py
@@ -12,7 +12,8 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import LinearSVC
 
-import model_training
+import error_analysis
+import evaluation
 import preprocessing
 
 # fixed random seed
@@ -20,22 +21,30 @@ RANDOM_SEED = 42
 np.random.seed(RANDOM_SEED)
 random.seed(RANDOM_SEED)
 
-
 MAX_ITERATIONS = 50000
+
+LOGISTIC_REGRESSION_NAME = "Logistic regression"
+SVM_NAME = "SVM"
 
 
 def main() -> None:
+    start_program_time = time.time()
+
+    # Preprocessing
+
     starting_time = time.time()
     train, dev, test = preprocessing.preprocessing(RANDOM_SEED)
     preprocessing_time = time.time() - starting_time
     print(f"Preprocessing took {preprocessing_time}s")
 
     starting_time = time.time()
-    train_x, train_y, dev_x, dev_y, test_x, test_y = (
-        preprocessing.tfidf_generator(train, dev, test)
+    train_x, train_y, dev_x, dev_y, test_x, test_y = preprocessing.tfidf_generator(
+        train, dev, test
     )
     vector_time = time.time() - starting_time
     print(f"Generating vectors took {vector_time}s")
+
+    # Training
 
     starting_time = time.time()
 
@@ -48,7 +57,7 @@ def main() -> None:
     test_prediction_regression = logistic_regression.predict(test_x)
 
     regression_time = time.time() - starting_time
-    print(f"Logistic regression took {regression_time}s")
+    print(f"{LOGISTIC_REGRESSION_NAME} took {regression_time}s")
 
     starting_time = time.time()
     svm = LinearSVC()
@@ -58,7 +67,25 @@ def main() -> None:
     dev_prediction_svm = svm.predict(dev_x)
     test_prediction_svm = svm.predict(test_x)
     svm_time = time.time() - starting_time
-    print(f"SVM took {svm_time}s")
+    print(f"{SVM_NAME} took {svm_time}s")
+
+    # Evaluation
+
+    evaluation.show_key_metrics(
+        test_y, test_prediction_regression, LOGISTIC_REGRESSION_NAME
+    )
+    evaluation.show_key_metrics(test_y, test_prediction_svm, SVM_NAME)
+
+    # Error analysis
+
+    error_analysis.show_error_analysis(
+        test_y, test_prediction_regression, LOGISTIC_REGRESSION_NAME
+    )
+    error_analysis.show_error_analysis(test_y, test_prediction_svm, SVM_NAME)
+
+    end_program_time = time.time()
+    duration_program = end_program_time - start_program_time
+    print(f"Total program duration: {duration_program}")
 
 
 if __name__ == "__main__":
